34138f13ce9e18a28346e0dfb2e22e2492ee89fd
seunayolu <seunayolu@gmail.com>
2024-03-07 15:06:48 -0600

    DevOps-Project-01-LiftAndShift

---
+++ userdata/ec2-instance-connect.sh
@@ -0,0 +1,4 @@
+mkdir /tmp/ec2-instance-connect
+curl https://amazon-ec2-instance-connect-us-west-2.s3.us-west-2.amazonaws.com/latest/linux_amd64/ec2-instance-connect.rpm -o /tmp/ec2-instance-connect/ec2-instance-connect.rpm
+curl https://amazon-ec2-instance-connect-us-west-2.s3.us-west-2.amazonaws.com/latest/linux_amd64/ec2-instance-connect-selinux.noarch.rpm -o /tmp/ec2-instance-connect/ec2-instance-connect-selinux.rpm
+sudo yum install -y /tmp/ec2-instance-connect/ec2-instance-connect.rpm /tmp/ec2-instance-connect/ec2-instance-connect-selinux.rpm
